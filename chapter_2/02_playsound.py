c42022dc12b48d661cfd9180e75ed75fd22c022e
code27012005 <neerajnoutiyal1322@gmail.com>
2024-07-22 13:51:43 +0530

    chapter_2

---
+++ chapter_2/02_playsound.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\Dheeraj\\Desktop\\python\\play.mp3.mp3\\play.mp3') 
---
+++ chapter_2/type_of_funvtion.py
@@ -0,0 +1,7 @@
+a =23
+b="34"
+c= 34.45
+
+input(type(a))
+input(type(b))
+input(type(c))
