5a2e2c676254a44b1c0cd17daa904d3c023ddade
code27012005 <neerajnoutiyal1322@gmail.com>
2024-07-24 21:36:28 +0530

    dsa

--- dsa_practice/binary_search_tree.py
+++ dsa_practice/binary_search_tree.py
@@ -1,69 +1,69 @@
 class Node:
-    def __init__(self,item=None,left=None,right=None):
-        self.item=item
-        self.left=left
-        self.right=right
+    def __init__(self, item=None, left=None, right=None):
+        self.item = item
+        self.left = left
+        self.right = right
 
 class bst:
     def __init__(self):
-        self.root=None
+        self.root = None
 
-    def insert(self,data):
-        self.root= self.rinsert(self.root,data)
-    def rinsert(self,root,data):
+    def insert(self, data):
+        self.root = self.rinsert(self.root, data)
+
+    def rinsert(self, root, data):
         if root is None:
             return Node(data)
         if data < root.item:
-            root.left=self.rinsert(root.left,data)
+            root.left = self.rinsert(root.left, data)
         elif data > root.item:
-            root.right=self.rinsert(root.right,data)
+            root.right = self.rinsert(root.right, data)
         return root
-    
-    def search(self,data):
-        self.root=self.rsearch(self.root,data)
 
-    def rsearch(self,root,data):
-       if root is None or root.item==data:
-           return data
-       if data < root.item:
-           return self.rsearch(root.left,data)
-       else:
-           return self.rsearch(root.right,data)
-       
+    def search(self, data):
+        return self.rsearch(self.root, data)
+
+    def rsearch(self, root, data):
+        if root is None or root.item == data:
+            return root
+        if data < root.item:
+            return self.rsearch(root.left, data)
+        return self.rsearch(root.right, data)
+
     def inorder(self):
-        result=[]
-        self.rinorder(self.root,result)
+        result = []
+        self.rinorder(self.root, result)
         return result
-    
-    def rinorder(self,root,result):
+
+    def rinorder(self, root, result):
         if root:
-            self.rinorder(root.left,result)
+            self.rinorder(root.left, result)
             result.append(root.item)
-            self.rinorder(root.right,result)
+            self.rinorder(root.right, result)
 
     def preorder(self):
-        result=[]
-        self.rpreorder(self.root,result)
+        result = []
+        self.rpreorder(self.root, result)
         return result
-    
-    def rpreorder(self,root,result):
+
+    def rpreorder(self, root, result):
         if root:
             result.append(root.item)
-            self.rpreorder(root.left,result)
-            self.rpreorder(root.right,result)
+            self.rpreorder(root.left, result)
+            self.rpreorder(root.right, result)
 
     def postorder(self):
-        result=[]
-        self.rpostorder(self.root,result)
+        result = []
+        self.rpostorder(self.root, result)
         return result
-    
-    def rpostorder(self,root,result):
+
+    def rpostorder(self, root, result):
         if root:
-            self.rpostorder(root.left,result)
-            self.rpostorder(root.right,result)
+            self.rpostorder(root.left, result)
+            self.rpostorder(root.right, result)
             result.append(root.item)
 
-b=bst()
+b = bst()
 b.insert(5)
 b.insert(3)
 b.insert(7)
@@ -76,4 +76,3 @@ b.insert(8)
 print("Inorder traversal:", b.inorder())
 print("Preorder traversal:", b.preorder())
 print("Postorder traversal:", b.postorder())
-           
